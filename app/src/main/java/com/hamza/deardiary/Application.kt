f929b49d4b74207949cc01435ff082600d25f440
hamza1311 <muhammadhamza1311@gmail.com>
2019-07-11 20:39:58 +0500

    Swapped 2 Room Databases for 1

--- app/src/main/java/com/hamza/deardiary/Application.kt
+++ app/src/main/java/com/hamza/deardiary/Application.kt
@@ -1,17 +1,16 @@
 package com.hamza.deardiary
 
 import android.app.Application
-import com.hamza.deardiary.arch.database.diaryitems.DiaryItemDatabase
+import com.hamza.deardiary.arch.database.DiaryDatabase
 import com.hamza.deardiary.arch.database.diaryitems.DiaryItemRepository
-import com.hamza.deardiary.arch.database.itemstag.ItemsTagDatabase
 import com.hamza.deardiary.arch.database.itemstag.ItemsTagRepository
 import com.hamza.deardiary.arch.repositories.MainDiaryItemRepository
 import com.hamza.deardiary.arch.repositories.MainItemTagRepository
 
 class App : Application() {
     val diaryItemRepository: DiaryItemRepository
-        get() = MainDiaryItemRepository(DiaryItemDatabase.getDatabase(this.applicationContext).diaryItemDao())
+        get() = MainDiaryItemRepository(DiaryDatabase.getDatabase(this.applicationContext).diaryItemDao())
 
     val itemTagRepository: ItemsTagRepository
-        get() = MainItemTagRepository(ItemsTagDatabase.getDatabase(this.applicationContext).tagsDao())
+        get() = MainItemTagRepository(DiaryDatabase.getDatabase(this.applicationContext).tagsDao())
 }
---
+++ app/src/main/java/com/hamza/deardiary/arch/database/DiaryDatabase.kt
@@ -0,0 +1,30 @@
+package com.hamza.deardiary.arch.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.hamza.deardiary.arch.database.diaryitems.DiaryItemDao
+import com.hamza.deardiary.arch.database.itemstag.ItemsTagDao
+import com.hamza.deardiary.arch.models.DiaryItem
+import com.hamza.deardiary.arch.models.ItemTag
+
+@Database(entities = [DiaryItem::class, ItemTag::class], version = 1)
+abstract class DiaryDatabase : RoomDatabase() {
+    abstract fun diaryItemDao(): DiaryItemDao
+    abstract fun tagsDao(): ItemsTagDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: DiaryDatabase? = null
+
+        fun getDatabase(context: Context): DiaryDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(context.applicationContext, DiaryDatabase::class.java, "dairy_database")
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
