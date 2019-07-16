6a972a6c21cfd409fb45fa60800ec50e25e968a1
hamza1311 <muhammadhamza1311@gmail.com>
2019-07-17 01:21:50 +0500

    Implemented some of settings UI

--- app/src/main/java/com/hamza/deardiary/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/hamza/deardiary/ui/fragments/SettingsFragment.kt
@@ -2,11 +2,36 @@ package com.hamza.deardiary.ui.fragments
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.material.snackbar.Snackbar
+import android.app.Activity
+import android.view.View
 import com.hamza.deardiary.R
-
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_xml, rootKey)
+
+        val setPasswordPref = findPreference("setPassword")
+
+        findPreference("enablePasswordPreference").apply {
+            setOnPreferenceChangeListener { _, newValue ->
+                showSnackbar(newValue.toString())
+                setPasswordPref.isEnabled = newValue as Boolean
+                true
+            }
+        }
+
+        setPasswordPref.apply {
+            setOnPreferenceChangeListener { _, newValue ->
+                showSnackbar(newValue.toString())
+                true
+            }
+        }
+    }
+}
+
+fun PreferenceFragmentCompat.showSnackbar(text: String) {
+    this.activity?.run {
+        Snackbar.make(this.findViewById<View>(android.R.id.content), text, Snackbar.LENGTH_LONG).show()
     }
 }
