6b0df9e2ef2fbe79669a18cbb981e886382ab967
hamza1311 <muhammadhamza1311@gmail.com>
2019-08-20 21:31:14 +0500

    Removed a freakin' Toast

--- app/src/main/java/com/hamza/deardiary/ui/fragments/TagItemsFragment.kt
+++ app/src/main/java/com/hamza/deardiary/ui/fragments/TagItemsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
@@ -39,7 +38,6 @@ class TagItemsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Toast.makeText(context, "${args.id}", Toast.LENGTH_LONG).show()
         diaryItemViewModel = obtainDiaryItemViewModel(DiaryItemViewModel::class.java)
         itemTagVewModel = obtainItemTagViewModel(ItemsTagViewModel::class.java)
         listAdapter = TagItemsListAdapter(context!!)
