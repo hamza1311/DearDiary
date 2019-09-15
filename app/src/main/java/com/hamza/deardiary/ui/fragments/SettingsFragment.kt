bba0e6c83f510abebae41ad94937003d367bc926
hamza1311 <muhammadhamza1311@gmail.com>
2019-09-15 17:44:40 +0500

    Attempted to add dark mode. UI looks bad in dark mode

--- app/src/main/java/com/hamza/deardiary/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/hamza/deardiary/ui/fragments/SettingsFragment.kt
@@ -1,6 +1,10 @@
 package com.hamza.deardiary.ui.fragments
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.DropDownPreference
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
@@ -41,6 +45,30 @@ class SettingsFragment : PreferenceFragmentCompat() {
         setSignaturePreference?.apply {
             setOnPreferenceChangeListener { _, _ ->
                 showSnackbar(getString(R.string.signature_set))
+                true
+            }
+        }
+
+        findPreference<DropDownPreference>("appTheme")?.apply {
+            setOnPreferenceChangeListener { preference, newValue ->
+                Log.d("ddd", "new val $newValue")
+                when (newValue.toString()) {
+                    "Light" -> {
+                        Log.d("ddd", "new val yayayy")
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    }
+                    "Dark" -> {
+                        Log.d("ddd", "new ddddval yayayy")
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    }
+                    else -> {
+                        if (Build.VERSION.SDK_INT >= 29) {
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                        } else {
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                        }
+                    }
+                }
                 true
             }
         }
