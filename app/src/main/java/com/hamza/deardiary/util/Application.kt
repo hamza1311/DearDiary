c23b89fa7294c78e83abea62b89f805e97e34aea
hamza1311 <muhammadhamza1311@gmail.com>
2019-09-19 18:52:17 +0500

    Dark mode improvements

--- app/src/main/java/com/hamza/deardiary/util/Application.kt
+++ app/src/main/java/com/hamza/deardiary/util/Application.kt
@@ -1,10 +1,11 @@
 package com.hamza.deardiary.util
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.hamza.deardiary.arch.database.DiaryDatabase
 import com.hamza.deardiary.arch.repositories.diaryitem.DiaryItemRepository
-import com.hamza.deardiary.arch.repositories.itemstag.ItemsTagRepository
 import com.hamza.deardiary.arch.repositories.diaryitem.MainDiaryItemRepository
+import com.hamza.deardiary.arch.repositories.itemstag.ItemsTagRepository
 import com.hamza.deardiary.arch.repositories.itemstag.MainItemTagRepository
 
 class App : Application() {
@@ -13,4 +14,9 @@ class App : Application() {
 
     val itemTagRepository: ItemsTagRepository
         get() = MainItemTagRepository(DiaryDatabase.getDatabase(this.applicationContext).tagsDao())
+
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES) // TODO: Change it to last selected option
+    }
 }
